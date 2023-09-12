68b616dbf81588fd6c912787530f89a557cd4ab9
MarkBrendan <markobinna65@gmail.com>
2023-09-12 12:34:43 -0700

    Sign

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase and alpha character.
+ *
+ * @c: c is an ascii character
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
