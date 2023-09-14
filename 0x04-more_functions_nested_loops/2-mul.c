4f508213ab36c71316bda12156a440fdfa9b5aca
MarkBrendan <markobinna65@gmail.com>
2023-09-14 12:05:08 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers.
+ *
+ * @a: arguement of the function
+ * @b: arguement of the function
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
