3c47df6321f76e182f1579c68c5b426182d5f433
MarkBrendan <markobinna65@gmail.com>
2023-09-18 14:53:03 -0700

    Write a function that prints a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: parameter to a pointer
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
