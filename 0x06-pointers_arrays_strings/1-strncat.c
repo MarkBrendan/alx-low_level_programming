78b848406b160b7558aa11f0a1b4b725660bbf44
MarkBrendan <markobinna65@gmail.com>
2023-09-21 15:51:44 -0700

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i, j;
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -20,16 +20,13 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	j = 0;
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
-
-	for (k = 0; k < n; k++)
-	{
-		dest[i + k] = src[k];
-	}
-	dest[i + k] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
