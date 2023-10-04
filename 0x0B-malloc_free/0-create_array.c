c995b1b02e2ca2d945adc281c9a75bce3af2fd98
MarkBrendan <markobinna65@gmail.com>
2023-10-04 11:54:34 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *j;
 
+	if (size == '\0')
+	{
+		return ('\0');
+	}
+
 	j = (char *) malloc(size * sizeof(char));
 
 	if (size == 0)
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - Write a function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as a parameter.
+ *
+ * @str: a copy of the string
+ *
+ *Return: returns NULL if str = 0
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *j;
+	int k;
+
+	if (*str == '\0')
+	{
+		return ('\0');
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	j = (char *) malloc((i + 1) * sizeof(char));
+
+	if (j == 0)
+	{
+		return ('\0');
+	}
+
+	for (k = 0; k <= i; k++)
+	{
+		j[k] = str[k];
+	}
+	return (j);
+}
