c7144e2022150ca9889a174e373c66094b73196c
MarkBrendan <markobinna65@gmail.com>
2023-11-15 00:10:19 -0800

    deleted some file to meet the requirement of deferment

--- 0x0D-preprocessor/0-object_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _0_object_like_macro_h
-#define _0_object_like_macro_h
-
-#define SIZE 1024
-
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _1_pi_h
-#define _1_pi_h
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif
