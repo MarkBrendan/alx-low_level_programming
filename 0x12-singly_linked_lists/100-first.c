50b4481860bafcd7ed72f45e4ba86cf12472c93e
MarkBrendan <markobinna65@gmail.com>
2023-12-10 11:21:59 -0800

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return : void
  */
-void premain(void) __attribute__((constructor))
+void __attribute__((constructor))premain(void)
 {
 	printf(
 			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
