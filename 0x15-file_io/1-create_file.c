68971581a5a16a34e158fad85bf32b4a6a7227cd
MarkBrendan <markobinna65@gmail.com>
2023-12-24 08:01:30 -0800

    Create a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	FP = open(filename, O_CREAT | O_WRONLY | O_TRUNC,  S_IRUSR | S_IWUSR);
+	FP = open(filename, O_CREAT | O_RDWR | O_TRUNC,  S_IRUSR | S_IWUSR);
 
 	if (FP == -1)
 		return (-1);
